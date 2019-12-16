ebdc6f730c9523b5e25ce367f58e328f1fd2f4e3
liyxi <mail.lyx@foxmail.com>
2019-12-16 16:18:25 +1100

    Update 1290_convert_binary_number_in_a_linked_list_to_integer.py

--- 1290_convert_binary_number_in_a_linked_list_to_integer.py
+++ 1290_convert_binary_number_in_a_linked_list_to_integer.py
@@ -1,5 +1,16 @@
-# pass in 28 ms
+# pass in 20 ms
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        return address.replace('.', '[.]')
+    def getDecimalValue(self, head: ListNode) -> int:
+        result = head.val
+        while head.next:
+            head = head.next
+            result = result << 1
+            result += head.val
+        return result
